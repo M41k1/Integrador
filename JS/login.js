64701d5cd83afaca42c54f4df2f7f8a73f101893
Angello Mallqui Diaz <angellomd16@gmail.com>
2025-04-10 23:32:45 -0500

    mejoras de diseño y codigo

--- JS/login.js
+++ JS/login.js
@@ -5,11 +5,10 @@ const formTitle = document.getElementById("form-title");
 
 toggleLink.addEventListener("click", (e) => {
   e.preventDefault();
-  
-  const isLoginVisible = loginForm.style.display !== "none";
 
-
-  if (isLoginVisible) {
+  // Quiero evitar hacer un montón de branching por usar el short-hand if else (a ? b : c)
+  // así que separé todo con una sola evaluación, no debería cambiar nada - Sebastián
+  if (loginForm.style.display !== "none") {
     loginForm.style.display = "none";
     registerForm.style.display = "block";
     formTitle.textContent =  "Crear Cuenta";
